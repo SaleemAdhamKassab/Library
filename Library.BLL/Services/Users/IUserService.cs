0ae46308101bc3e4c0555dbcef41d37394a49258
Administrator <Administrator@DESKTOP-IF8JJLJ>
2024-04-29 08:39:04 +0300

    update

--- Library.BLL/Services/Users/IUserService.cs
+++ Library.BLL/Services/Users/IUserService.cs
@@ -4,7 +4,6 @@ namespace Library.BLL.Services.Users
 {
     public interface IUserService
     {
-		//bool loginResult(string email, string password);
 		User checkUserCredentials(string email, string password);
 	}
 }
